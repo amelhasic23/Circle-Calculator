a4730bd43f7788143d5599e8390cd584ad3a391e
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-18 15:26:16 +0100

    Commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercises
+{
+    class CircleArea
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            double r = 5;
+            double p = (r * r) * PI;
+
+            Console.WriteLine(p);
+        }
+    }
+}
